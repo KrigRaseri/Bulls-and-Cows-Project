5066c4ae445fac999b3951feab92878de6f515c6
Krig Raseri <krigraseri@gmail.com>
2023-02-15 05:06:12 -0600

    Update game
    Turned the main game logic into a method that calls randomCode to create the random code to be guessed.

--- bullsandcows/src/bullsandcows/Main.java
+++ bullsandcows/src/bullsandcows/Main.java
@@ -1,49 +1,8 @@
 package bullsandcows;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        Scanner sc = new Scanner(System.in);
-
-        boolean win = false;
-        String num = "1234";
-
-
-        while (!win) {
-            System.out.print("> ");
-            String inp = sc.nextLine();
-            int cows = 0;
-            int bulls = 0;
-
-            for (int i = 0; i < inp.length(); i++) {
-                if (inp.charAt(i) == num.charAt(i)) {
-                    bulls++;
-                }
-
-                if (inp.charAt(i) != num.charAt(i) && num.indexOf(inp.charAt(i)) >= 0) {
-                    cows++;
-                }
-            }
-
-            if (bulls == 4) {
-                win = true;
-                cows = 0;
-            }
-
-            if (cows == 0 && bulls == 0) {
-                System.out.printf("Grade: None. The secret code is %s.\n", num);
-            }
-
-            else {
-            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.\n", bulls, cows, num);
-            }
-
-        } */
-
-
-        System.out.printf("The random secret number is %s.\n", Util.randomCode());
-
+        Util.gameInit();
     }
 }
--- bullsandcows/src/bullsandcows/Util.java
+++ bullsandcows/src/bullsandcows/Util.java
@@ -7,6 +7,7 @@ public class Util {
         Random rand = new Random();
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Please, enter the secret code's length:");
         System.out.print("> ");
         int size = sc.nextInt();
 
@@ -35,5 +36,54 @@ public class Util {
 
 
         return code;
+    }
+
+
+
+    public static void gameInit() {
+        Scanner sc = new Scanner(System.in);
+
+        boolean win = false;
+        String num = Util.randomCode();
+        int turns = 1;
+        System.out.println("Okay, let's start a game!");
+
+
+
+        while (true) {
+            System.out.println("Turn " + turns);
+            System.out.print("> ");
+            String inp = sc.nextLine();
+            int cows = 0;
+            int bulls = 0;
+            turns++;
+
+
+            for (int i = 0; i < inp.length(); i++) {
+                if (inp.charAt(i) == num.charAt(i)) {
+                    bulls++;
+                }
+
+                if (inp.charAt(i) != num.charAt(i) && num.indexOf(inp.charAt(i)) >= 0) {
+                    cows++;
+                }
+            }
+
+
+
+            if (bulls == num.length()) {
+                System.out.printf("Grade: %d bulls\n", bulls);
+                System.out.println("Congratulations! You guessed the secret code.");
+                break;
+            }
+
+            if (cows == 0 && bulls == 0) {
+                System.out.println("Grade: None.");
+            }
+
+            else {
+                System.out.printf("Grade: %d bull(s) and %d cow(s).\n", bulls, cows);
+            }
+        }
     }
 }
