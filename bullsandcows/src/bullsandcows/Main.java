6f6407b46cfe6076a9cc0448b0cb9960b5a7c895
Krig Raseri <krigraseri@gmail.com>
2023-02-16 03:46:09 -0600

    Update comments
    Updates the comments and documentations for more detail.

--- bullsandcows/src/bullsandcows/Main.java
+++ bullsandcows/src/bullsandcows/Main.java
@@ -1,30 +1,16 @@
 package bullsandcows;
 
-
-import java.util.Scanner;
-
 public class Main extends Util {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Please, enter the secret code's length:");
-        System.out.print("> ");
-        int size = sc.nextInt();
+        //Sets and checks size and nSymbols.
+        int size = setSize();
+        int nSymbols = setSymbols();
 
-        System.out.println("Input the number of possible symbols in the code:");
-        System.out.print("> ");
-        int nSymbols = sc.nextInt();
-
-
-
-        while (size <= 0 || size >= 36) {
-            System.out.println("Error: input incorrect.");
-            size = sc.nextInt();
-            System.out.println("> ");
-
-        }
-
+        //Creates the secret code.
         String secretCode = randomCode(size, nSymbols);
+
+        //Starts game.
         gameInit(secretCode, nSymbols);
 
     }
--- bullsandcows/src/bullsandcows/Util.java
+++ bullsandcows/src/bullsandcows/Util.java
@@ -3,34 +3,47 @@ package bullsandcows;
 import java.util.*;
 
 public class Util {
-    public static int sizeCheck(int size) {
-        Scanner sc = new Scanner(System.in);
-        int cSize = size;
 
-        while (cSize <= 0 || cSize >= 36) {
-            System.out.println("Error: input incorrect.");
-            cSize = sc.nextInt();
-            System.out.println("> ");
+    //Sets int size, then checks its validity.
+    public static int setSize() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please, enter the secret code's length:");
+        System.out.print("> ");
+        int size = sc.nextInt();
+
+        while (size <= 0 || size > 36) {
+            System.out.println("Error: input incorrect, try again.");
+            System.out.print("> ");
+            size = sc.nextInt();
         }
 
-        return cSize;
+        return size;
     }
 
 
-    public static int symbolCheck(int nSymbol) {
+    //Sets int nSymbols, then checks its validity.
+    public static int setSymbols() {
         Scanner sc = new Scanner(System.in);
-        int cSym = nSymbol;
+        System.out.println("Input the number of possible symbols in the code:");
+        System.out.print("> ");
+        int nSymbols = sc.nextInt();
 
-        while (cSym <= 0 || cSym >= 36) {
-            System.out.println("Error: input incorrect.");
-            cSym = sc.nextInt();
-            System.out.println("> ");
+        while (nSymbols <= 0 || nSymbols > 36) {
+            System.out.println("Error: input incorrect, try again.");
+            System.out.print("> ");
+            nSymbols = sc.nextInt();
         }
 
-        return cSym;
+        return nSymbols;
     }
 
 
+    /**
+     * Prints out a blurred version of the code ***** and the number, as well as letters, used in the secret code.
+     *
+     * @param code represents the secret code already made.
+     * @param nSymbols chosen amount of symbols to be used in secret code (based on a range of 10-36).
+     * */
     public static String printInfo(String code, int nSymbols) {
         String range = "0123456789abcdefghijklmnopqrstuvwxyz";
         String stars = code.replaceAll("(?s).", "*");
@@ -39,9 +52,11 @@ public class Util {
 
         if (nSymbols == 11) {
             section = "(0-9, a)";
+
         } else if (nSymbols > 11) {
             letter = range.charAt(nSymbols - 1);
             section = String.format("(0-9, a-%c)", letter);
+
         } else {
             section = "(0-9)";
         }
@@ -51,10 +66,14 @@ public class Util {
 
     }
 
-
+    /**
+     * Method to create the secret code that needs to be guessed.
+     *
+     * @param size length of the secret code.
+     * @param nSymbols chosen amount of symbols to be used in secret code (based on a range of 10-36).
+     * */
     public static String randomCode(int size, int nSymbols) {
         Random rand = new Random();
-
         String code = "";
         LinkedHashSet<Character> set = new LinkedHashSet<>();
         String range = "0123456789abcdefghijklmnopqrstuvwxyz";
@@ -65,22 +84,25 @@ public class Util {
                 set.add(c.charAt(0));
 
             } else {
-
                 set.add(range.charAt(rand.nextInt(36)));
-            }
 
+            }
         }
 
         for (Character c : set) {
             code += c.toString();
         }
 
-        System.out.println(code);
         return code;
     }
 
 
-
+    /**
+     * Game initialize starts the game, and controls the win condition.
+     *
+     * @param code represents the already created secret code to be guessed.
+     * @param nSymbol represents the chosen potential numbers and characters that can be in the secret code.
+     * */
     public static void gameInit(String code, int nSymbol) {
         Scanner sc = new Scanner(System.in);
         int turns = 1;
@@ -95,29 +117,31 @@ public class Util {
             int bulls = 0;
             turns++;
 
-
+            //If symbol is in correct place +1 to bulls.
             for (int i = 0; i < inp.length(); i++) {
                 if (inp.charAt(i) == code.charAt(i)) {
                     bulls++;
                 }
 
+                //If symbol is present, but not in the right place +1 to cows.
                 if (inp.charAt(i) != code.charAt(i) && code.indexOf(inp.charAt(i)) >= 0) {
                     cows++;
                 }
             }
 
-
-
+            //Win condition.
             if (bulls == code.length()) {
                 System.out.printf("Grade: %d bulls\n", bulls);
                 System.out.println("Congratulations! You guessed the secret code.");
                 break;
             }
 
+            //No cows or bulls found then none.
             if (cows == 0 && bulls == 0) {
                 System.out.println("Grade: None.");
             }
 
+            //Finally, display amount of bulls and cows.
             else {
                 System.out.printf("Grade: %d bull(s) and %d cow(s).\n", bulls, cows);
             }
